cb3d5b8dc91599d87fda1a286995e7c42249ddce
That One Cheater <lifertxcode@gmail.com>
2022-07-15 03:01:39 -0700

    Speed modes

--- src/main/java/team/gravityrecode/clientbase/impl/module/movement/Speed.java
+++ src/main/java/team/gravityrecode/clientbase/impl/module/movement/Speed.java
@@ -5,34 +5,28 @@ import team.gravityrecode.clientbase.api.eventBus.EventHandler;
 import team.gravityrecode.clientbase.api.moduleBase.Module;
 import team.gravityrecode.clientbase.api.moduleBase.ModuleInfo;
 import team.gravityrecode.clientbase.impl.event.player.PlayerMoveEvent;
+import team.gravityrecode.clientbase.impl.module.movement.speed.NCP;
+import team.gravityrecode.clientbase.impl.module.movement.speed.OldNCP;
+import team.gravityrecode.clientbase.impl.property.ModeSetting;
 import team.gravityrecode.clientbase.impl.util.util.player.MovementUtil;
 
 @ModuleInfo(moduleName = "Speed", moduleCategory = Module.ModuleCategory.MOVEMENT, moduleKeyBind = Keyboard.KEY_F)
 public class Speed extends Module {
 
-    private double moveSpeed;
-    private boolean doSlow;
+    OldNCP oldNCP = new OldNCP(this, "OldNCP");
+    NCP ncp = new NCP(this, "NCP");
+    public ModeSetting modeSetting = new ModeSetting(this, "Mode", oldNCP, ncp);
 
     @EventHandler
     public void onMove(PlayerMoveEvent event) {
-        if (MovementUtil.isMovingOnGround()) {
-            moveSpeed = MovementUtil.getBaseMoveSpeed() * 1.8;
-            event.setY(mc.thePlayer.motionY = 0.42F);
-            doSlow = true;
-        } else {
-            if (doSlow) {
-                moveSpeed -= 0.72 * (moveSpeed - MovementUtil.getBaseMoveSpeed());
-                doSlow = false;
-            } else {
-                moveSpeed -= moveSpeed / 159;
-            }
-        }
-        MovementUtil.setSpeed(event, moveSpeed);
+      oldNCP.onMove(event);
+      ncp.onMove(event);
     }
 
     @Override
     public void onDisable() {
-        moveSpeed = 0;
+        oldNCP.onDisable();
+        ncp.onDisable();
         super.onDisable();
     }
 }
---
+++ src/main/java/team/gravityrecode/clientbase/impl/module/movement/speed/NCP.java
@@ -0,0 +1,40 @@
+package team.gravityrecode.clientbase.impl.module.movement.speed;
+
+import team.gravityrecode.clientbase.api.client.IToggleable;
+import team.gravityrecode.clientbase.api.eventBus.EventHandler;
+import team.gravityrecode.clientbase.impl.event.player.PlayerMoveEvent;
+import team.gravityrecode.clientbase.impl.property.mode.Mode;
+import team.gravityrecode.clientbase.impl.util.util.player.MovementUtil;
+
+public class NCP extends Mode {
+
+    double moveSpeed;
+    boolean doSlow;
+
+    public NCP(IToggleable owner, String name) {
+        super(owner, name);
+    }
+
+    @EventHandler
+    public void onMove(PlayerMoveEvent event) {
+        if (MovementUtil.isMovingOnGround()) {
+            moveSpeed = MovementUtil.getBaseMoveSpeed() * 1.8;
+            event.setY(mc.thePlayer.motionY = 0.42F);
+            doSlow = true;
+        } else {
+            if (doSlow) {
+                moveSpeed -= 0.72 * (moveSpeed - MovementUtil.getBaseMoveSpeed());
+                doSlow = false;
+            } else {
+                moveSpeed -= moveSpeed / 159;
+            }
+        }
+        MovementUtil.setSpeed(event, moveSpeed);
+    }
+
+    @Override
+    public void onDisable() {
+        moveSpeed = 0;
+        super.onDisable();
+    }
+}
